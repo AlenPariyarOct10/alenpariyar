e5211ee316680574393dbcea30d77432cd8234e4
Alen Pariyar <80625066+AlenPariyarOct10@users.noreply.github.com>
2022-07-24 23:01:46 +0545

    js upload
    js for download

---
+++ script.js
@@ -0,0 +1,13 @@
+function ddllfile(){
+    let person = prompt("Password");
+let text;
+if (person == "rrbca123") {
+    window.location.href = "https://drive.google.com/drive/folders/1Ex7h7CiABZum4s_MI69hjwkG0BPct8wa?usp=sharing";
+  
+  
+} else {
+    text = "Sorry ! Access Denied ";
+    alert("❌"+text);
+}
+console.log(text);
+}
