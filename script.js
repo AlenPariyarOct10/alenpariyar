ce020bed916f9dcdb73e65f9a5765e28d0410f71
Alen Pariyar <80625066+AlenPariyarOct10@users.noreply.github.com>
2022-07-25 09:52:58 +0545

    change123

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function ddllfile(){
-    let person = prompt("Password");
+    let person = prompt("Password"); 
 let text;
 if (person == "rrbca123") {
     window.location.href = "https://drive.google.com/drive/folders/1Ex7h7CiABZum4s_MI69hjwkG0BPct8wa?usp=sharing";
