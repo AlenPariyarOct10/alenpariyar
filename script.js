21b3150e92ef8cc71ecd7bce6acd0c1a74defbc1
Alen Pariyar <80625066+AlenPariyarOct10@users.noreply.github.com>
2022-07-25 10:18:33 +0545

    added multilink

--- script.js
+++ script.js
@@ -3,25 +3,12 @@ function ddllfile(){
 let text;
 if (person == "rrbca123") {
     window.location.href = "https://drive.google.com/drive/folders/1Ex7h7CiABZum4s_MI69hjwkG0BPct8wa?usp=sharing";
-  
+  window.open("https://www.mediafire.com/file/y9kq56vji2rmeww/CFA.zip/file","_blank");
   
 } else {
     text = "Sorry ! Access Denied ";
     alert("❌"+text);
-}
-console.log(text);
 }
 
-function ddllfile1(){
-    let person = prompt("Password");
-let text;
-if (person == "rrbca123") {
-    window.location.href = "https://www.mediafire.com/file/y9kq56vji2rmeww/CFA.zip/file";
-  
-  
-} else {
-    text = "Sorry ! Access Denied ";
-    alert("❌"+text);
 }
-console.log(text);
-}
+
